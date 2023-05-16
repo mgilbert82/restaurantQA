b38e57f89c6cf9d7386258b70ed6232ab50b73a8
Michel GILBERT <michel.1.gilbert@arval.fr>
2023-05-16 10:50:11 +0200

    add reservation entity

---
+++ src/Entity/Reservation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $hour = null;
+
+    #[ORM\Column]
+    private ?int $numberOfReservation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getHour(): ?\DateTimeInterface
+    {
+        return $this->hour;
+    }
+
+    public function setHour(\DateTimeInterface $hour): self
+    {
+        $this->hour = $hour;
+
+        return $this;
+    }
+
+    public function getNumberOfReservation(): ?int
+    {
+        return $this->numberOfReservation;
+    }
+
+    public function setNumberOfReservation(int $numberOfReservation): self
+    {
+        $this->numberOfReservation = $numberOfReservation;
+
+        return $this;
+    }
+}
